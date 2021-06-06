778e2b782cdc13a89a6e46ae5e553cf3d558a865
nabesuke <nabesuke5098@gmail.com>
2021-06-06 12:55:42 +0900

    [modified]Blog.php & User.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $blogs = Blog::with('user')->get();
+        $blogs = Blog::with('user:id,name')->get();
+
+        // return $blogs;
 
         return view('home', compact('blogs'));
     }
