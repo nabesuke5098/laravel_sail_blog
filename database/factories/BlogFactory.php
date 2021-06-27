64fdc4952b0f94732dd79fc90b9d7483dcd65cc5
nabesuke <nabesuke5098@gmail.com>
2021-06-28 08:41:45 +0900

    [add]new column 'is_open'

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -24,6 +24,7 @@ class BlogFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
+            'is_open' => $this->faker->randomElement([true, true, true, true, false]),
             'title' => $this->faker->realText(20),
             'body'  => $this->faker->realText(100),
             'updated_at' => $this->faker->dateTimeBetween('-10days', '0days'),
