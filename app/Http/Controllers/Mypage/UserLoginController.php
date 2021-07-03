2f69621888f78ea25815397d616074edb6bb7b2e
nabesuke <nabesuke5098@gmail.com>
2021-07-04 08:40:39 +0900

    [add]login

--- app/Http/Controllers/Mypage/UserLoginController.php
+++ app/Http/Controllers/Mypage/UserLoginController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers\Mypage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
 {
     public function index()
     {
         return view('mypage.login');
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required', 'email:filter'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (Auth::attempt($data)) {
+            $request->session()->regenerate();
+
+            return redirect('mypage');
+        }
+
+        return back()->withErrors([
+            'email' => 'メールアドレスまたはパスワードが間違っています。',
+        ]);
     }
 }
